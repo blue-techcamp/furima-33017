3b0d124766de1144389dfeed811a95cc3d9681bd
blue-techcamp <aoitaiga78@gmail.com>
2021-01-11 17:31:10 +0900

    バリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 end
