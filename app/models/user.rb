3473894d4996315f1806e0d5d54f600f204462d8
blue-techcamp <aoitaiga78@gmail.com>
2021-01-13 13:24:08 +0900

    名字・名前は全角入力、 フリガナはカタカナ入力必須のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,14 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
