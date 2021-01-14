4800da45734cfb7a87fd77b4e6a03d8bb7a03c93
blue-techcamp <aoitaiga78@gmail.com>
2021-01-14 18:20:50 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  #空の投稿を保存できないようにする
+  validates :info, :price, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
 end
