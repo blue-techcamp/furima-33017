b246b404d249bc12a391130a382dcbf087976a1c
blue-techcamp <aoitaiga78@gmail.com>
2021-01-15 23:11:22 +0900

    商品出品情報保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,12 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id)
   end
 
   def configure_permitted_parameters
