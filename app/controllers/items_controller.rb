7cb0e74fb4c880d21e7b0513bd7858622a832099
blue-techcamp <aoitaiga78@gmail.com>
2021-01-16 23:47:50 +0900

    不要なconfigure_permitted_parametersメソッドの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id)
   end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:new_item, keys: [:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id])
-  end
 end
