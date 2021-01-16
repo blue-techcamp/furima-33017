df48bf7d7c1acb4112e0a205321111ba67bc5d36
blue-techcamp <aoitaiga78@gmail.com>
2021-01-16 23:51:16 +0900

    priceをstring型に変更

--- db/migrate/20210113092721_create_items.rb
+++ db/migrate/20210113092721_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.references :user,foreign_key: true, null: false
       t.string :name, null: false
       t.text :info, null: false
-      t.integer :price, null: false
+      t.string :price, null: false
       t.integer :category_id, null: false
       t.integer :sale_status_id, null: false
       t.integer :shipping_fee_status_id, null: false
