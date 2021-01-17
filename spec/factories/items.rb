75d2e62dcc25465a11f6287b7f89c6e2a97278cb
blue-techcamp <aoitaiga78@gmail.com>
2021-01-17 18:01:18 +0900

    不要な""の削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :item do
-    user_id {"2"}
+    association :user
     name {"商品名"}
     info {"商品の説明"}
     price {"9999999"}
-    category_id {"2"}
-    sale_status_id {"2"}
-    shipping_fee_status_id {"2"}
-    prefecture_id {"2"}
-    scheduled_delivery_id {"2"}
+    category_id {2}
+    sale_status_id {2}
+    shipping_fee_status_id {2}
+    prefecture_id {2}
+    scheduled_delivery_id {2}
   end
 
   after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,31 +38,31 @@ RSpec.describe Item, type: :model do
       end
 
       it "category_idが1では登録できないこと" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it "sale_status_idが1では登録できないこと" do
-        @item.sale_status_id = "1"
+        @item.sale_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Sale status must be other than 1")
       end
 
       it "shipping_fee_status_idが1では登録できないこと" do
-        @item.shipping_fee_status_id = "1"
+        @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee status must be other than 1")
       end
 
       it "prefecture_idが1では登録できないこと" do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it "scheduled_delivery_idが1では登録できないこと" do
-        @item.scheduled_delivery_id = "1"
+        @item.scheduled_delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
       end
