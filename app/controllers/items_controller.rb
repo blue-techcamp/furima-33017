c3506a8eebc5a71216beb37775f7965876c88943
blue-techcamp <aoitaiga78@gmail.com>
2021-01-17 18:02:11 +0900

    user_idをそぞんできるように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id)
+    params.require(:item).permit(:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
   end
 end
