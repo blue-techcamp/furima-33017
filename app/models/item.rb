f2ecc0e26636ef2a7c0f285341ccb5a08b933b31
blue-techcamp <aoitaiga78@gmail.com>
2021-01-17 11:13:51 +0900

    active_hashを用いた箇所の不要な、空不可バリデーションの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to :category, :status, :shipping_fee, :prefecture, :scheduled
 
   with_options presence: true do
-    validates :image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id
+    validates :image, :name, :info, :price
   end
 
   #ジャンルの選択が「--」の時は保存できないようにする
