6d9c12991b5bbfbd2f9a568967d67396256a08ff
blue-techcamp <aoitaiga78@gmail.com>
2021-01-17 11:14:47 +0900

    active_hashを用いた箇所の単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,34 +37,34 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it "category_idが空では登録できないこと" do
-        @item.category_id = ""
+      it "category_idが1では登録できないこと" do
+        @item.category_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
-      it "sale_status_idが空では登録できないこと" do
-        @item.sale_status_id = ""
+      it "sale_status_idが1では登録できないこと" do
+        @item.sale_status_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sale status can't be blank", "Sale status is not a number")
+        expect(@item.errors.full_messages).to include("Sale status must be other than 1")
       end
 
-      it "shipping_fee_status_idが空では登録できないこと" do
-        @item.shipping_fee_status_id = ""
+      it "shipping_fee_status_idが1では登録できないこと" do
+        @item.shipping_fee_status_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank", "Shipping fee status is not a number")
+        expect(@item.errors.full_messages).to include("Shipping fee status must be other than 1")
       end
 
-      it "prefecture_idが空では登録できないこと" do
-        @item.prefecture_id = ""
+      it "prefecture_idが1では登録できないこと" do
+        @item.prefecture_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
-      it "scheduled_delivery_idが空では登録できないこと" do
-        @item.scheduled_delivery_id = ""
+      it "scheduled_delivery_idが1では登録できないこと" do
+        @item.scheduled_delivery_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank", "Scheduled delivery is not a number")
+        expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
       end
 
     end
