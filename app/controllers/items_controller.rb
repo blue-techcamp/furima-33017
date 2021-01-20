7443838aecc33d1044e9569404f911fc7c6006f7
blue-techcamp <75652738+blue-techcamp@users.noreply.github.com>
2021-01-20 19:59:28 +0900

    updateアクションのルーティングを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
