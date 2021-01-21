8c2e1f7483a92ebcbd34f1378b23f4ed0876bf3f
blue-techcamp <75652738+blue-techcamp@users.noreply.github.com>
2021-01-21 21:43:47 +0900

    destroyアクションのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :info, :price, :category_id, :sale_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
