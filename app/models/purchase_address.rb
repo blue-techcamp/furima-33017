e4b80e5765162b8c1cbffe65075c610e707290b0
blue-techcamp <75652738+blue-techcamp@users.noreply.github.com>
2021-01-29 14:04:20 +0900

    不要な物を削除

--- app/models/purchase_address.rb
+++
@@ -1,4 +0,0 @@
-class PurchaseAddress
-  include ActiveModel::Model
-  attr_accessor :purchase_record, :postal_code, :prefecture_id, :city, :prefecture, :city, :house_num, :building_name, :phone_num, :user, :item
-end
