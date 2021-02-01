db65fd225d515947bd5346a7f04865365602956a
blue-techcamp <75652738+blue-techcamp@users.noreply.github.com>
2021-02-01 15:58:51 +0900

    公開鍵と共にカード情報をPAY.JP側へ送る

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_69ad3a5f67ab2ad7b512b5ef"); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_form[number]"),
+      cvc: formData.get("purchase_form[cvc]"),
+      exp_month: formData.get("purchase_form[exp_month]"),
+      exp_year: `20${formData.get("purchase_form[exp_year]")}`,
+    };
+    console.log(card)
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
