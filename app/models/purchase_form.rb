1f7c5f61dd559d70da9e76e9a47c9c4f85244cea
blue-techcamp <75652738+blue-techcamp@users.noreply.github.com>
2021-02-01 19:21:46 +0900

    トークンをサーバーサイドへ送る

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,6 +1,6 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :order_id, :postal_code, :prefecture_id, :city, :prefecture, :house_num, :building_name, :phone_num, :user_id, :item_id
+  attr_accessor :order_id, :postal_code, :prefecture_id, :city, :prefecture, :house_num, :building_name, :phone_num, :user_id, :item_id, :token
 
   # 「電話番号」の郵便番号に関するバリデーション
   validates :phone_num, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters." }
@@ -20,6 +20,6 @@ class PurchaseForm
     # 各テーブルにデータを保存する処理を書く
         # 住所の情報を保存
         order = Order.create(user_id: user_id, item_id: item_id)
-        Address.create!(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_num: house_num, building_name: building_name, phone_num: phone_num)
+        Address.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_num: house_num, building_name: building_name, phone_num: phone_num)
   end
 end
